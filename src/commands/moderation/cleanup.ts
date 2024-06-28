6dd041bf3f492ef922f472fbcca8b85318d93485
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-06-28 12:59:05 +0530

    added role safety to the clenaup command

--- src/commands/moderation/cleanup.ts
+++ src/commands/moderation/cleanup.ts
@@ -4,6 +4,7 @@ import {
   ApplicationCommandOptionType,
   TextChannel,
   CommandInteractionOptionResolver,
+  GuildMember,
 } from "discord.js";
 import dotenv from "dotenv";
 
@@ -11,6 +12,7 @@ dotenv.config();
 
 const DAILYPROGRESS_CHANNEL_ID = process.env.DAILYPROGRESS_CHANNEL_ID!;
 const STATUS_CHANNEL_ID = process.env.STATUS_CHANNEL_ID!;
+const ADMIN_ROLE_ID = process.env.ADMIN_ROLE_ID!;
 
 module.exports = {
   name: "clear",
@@ -41,6 +43,15 @@ module.exports = {
   callback: async (client: Client, interaction: CommandInteraction) => {
     const options = interaction.options as CommandInteractionOptionResolver;
     const prompt = options.getString("prompt");
+    const member = interaction.member as GuildMember;
+
+    if (!member.roles.cache.has(ADMIN_ROLE_ID)) {
+      await interaction.reply({
+        content: "You do not have permission to use this command.",
+        ephemeral: true,
+      });
+      return;
+    }
 
     let channelId: string | null = null;
     if (prompt === "d") {
