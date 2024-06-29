9c0320e169d8c2de4b6b06faef19fd7e3cfaa1dc
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2024-06-29 14:15:46 +0530

    add channel option

--- src/commands/moderation/announce.ts
+++ src/commands/moderation/announce.ts
@@ -19,6 +19,12 @@ export const options = [
     type: ApplicationCommandOptionType.String,
     required: true,
   },
+  {
+    name: "channel",
+    description: "Channel to send the announcement",
+    type: ApplicationCommandOptionType.Channel,
+    required: false,
+  },
 ];
 export const deleted = false;
 
@@ -38,7 +44,17 @@ export async function callback(
   }
   await interaction.deferReply();
   const message = interaction.options.get("message")!.value as string;
-
+  if (interaction.options.get("channel")) {
+    const channel = interaction.options.get("channel")!.channel!.id;
+    const announcementChannel = client.channels.cache.get(
+      channel
+    ) as TextChannel;
+    announcementChannel!.send(message);
+    await interaction.editReply({
+      content: "Announcement sent!",
+    });
+    return;
+  }
   const announcementChannelID = process.env.ANNOUNCEMENT_CHANNEL_ID;
   const announcementChannel = client.channels.cache.get(
     announcementChannelID!
